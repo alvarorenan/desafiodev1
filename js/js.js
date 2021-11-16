d01bf32e3d006952f6ffad4905af24f20c895a89
Álvaro Renan <alvaroca1544@gmail.com>
2021-11-16 17:47:16 -0300

    data

--- js/js.js
+++ js/js.js
@@ -1,3 +1,22 @@
+// Obtém a data/hora atual
+var data = new Date();
+
+// Guarda cada pedaço em uma variável
+var dia     = data.getDate();           // 1-31
+var dia_sem = data.getDay();            // 0-6 (zero=domingo)
+var mes     = data.getMonth();          // 0-11 (zero=janeiro)
+var ano2    = data.getYear();           // 2 dígitos
+var ano4    = data.getFullYear();       // 4 dígitos
+var hora    = data.getHours();          // 0-23
+var min     = data.getMinutes();        // 0-59
+var seg     = data.getSeconds();        // 0-59
+var mseg    = data.getMilliseconds();   // 0-999
+var tz      = data.getTimezoneOffset(); // em minutos
+
+// Formata a data e a hora (note o mês + 1)
+var str_data = dia + '/' + (mes+1) + '/' + ano4;
+var str_hora = hora + ':' + min + ':' + seg;
+
 
 
 
@@ -16,7 +35,7 @@ function adicionar(){
     }
     if(verif){
         id++;
-        lista += "<li id='"+id+"' style='margin-bottom: 25px; border-top-width: 1px;' class='list-group-item'> <h4>" + titulo + "</h4>" + text  + "<br>"
+        lista += "<li id='"+id+"' style='margin-bottom: 25px; border-top-width: 1px; background-color: #c0c0c094' class='list-group-item'> <h3 style='text-align: center; font-weight: bold;'>" + titulo + "</h3>"+"<p style='text-align: right; font-style: italic; font-size: 13px;'> Publicado em "+ str_data+ " às "+ str_hora+ " por Álvaro Renan, Discente UFRN." + "</p>" + text  + "<br>"
         + "</li>";
         
         document.getElementById("lista").innerHTML = lista;
